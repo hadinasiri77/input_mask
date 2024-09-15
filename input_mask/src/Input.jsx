2193e00a23d33bedd4a07ceda456685f87faa8f6
Hadi Nasiri <hadinasiri77@gmail.com>
2024-09-15 19:42:19 +0330

    input_task

---
+++ input_mask/src/Input.jsx
@@ -0,0 +1,17 @@
+const Input = ({handleChange, hint,error}) => {
+  return (
+    <div className="input">
+      <label htmlFor="input" >
+        {hint}
+      </label>
+      <input
+        type="text"
+        id="input"
+        onChange={handleChange}
+      />
+    {error&&<p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
